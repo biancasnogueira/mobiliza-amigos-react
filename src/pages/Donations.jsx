af43b92d5ace189c1b251c505e9757b7f93f4f9a
Bianca Nogueira <nogbianca@outlook.com>
2024-11-29 08:06:35 -0300

    Adicionando components e pages

---
+++ src/pages/Donations.jsx
@@ -0,0 +1,10 @@
+function Donations() {
+    return (
+        <>
+            <h1>Donations</h1>
+            <p>Subtitle</p>
+        </>
+    )
+}
+
+export default Donations
---
+++ src/pages/Register.jsx
@@ -0,0 +1,10 @@
+function Register() {
+    return (
+        <>
+            <h1>Register</h1>
+            <p>Subtitle</p>
+        </>
+    )
+}
+
+export default Register
