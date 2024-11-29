903645ee1b7f2299225cba013ca85b5af20a68f8
Bianca Nogueira <nogbianca@outlook.com>
2024-11-29 09:44:05 -0300

    Adicionando footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,47 +1,56 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-//ASSETS
-import './Footer.css'
-import LogoInstagram from '../../assets/logo-instagram.svg'
-import LogoTiktok from '../../assets/logo-tiktok.svg'
-import FooterMobiliza from '../../assets/footer-mobiliza.svg'
+// ASSETS
+import './Footer.css';
+import LogoInstagram from '../../assets/logo-instagram.svg';
+import LogoTiktok from '../../assets/logo-tiktok.svg';
+import FooterMobiliza from '../../assets/footer-mobiliza.svg';
 
 function Footer() {
-
-    return (
-
+  return (
     <footer>
-    <div class="footer-container">
-        <div class="footer-column logo-column">
-            <img src={footer-mobiliza.svg} alt="Mobiliza Amigos" />
+      <div className="footer-container">
+        <div className="footer-column logo-column">
+          <img src={FooterMobiliza} alt="Mobiliza Amigos" />
         </div>
-        <div class="footer-column links-column">
-            <a href="#">Quem somos</a><br />
-            <a href="#">Doações</a><br />
-            <a href="#">Fale conosco</a>
+        <div className="footer-column links-column">
+          <a href="#">Quem somos</a>
+          <br />
+          <a href="#">Doações</a>
+          <br />
+          <a href="#">Fale conosco</a>
         </div>
-        <div class="footer-column contacts-column">
-            <h4>Contatos</h4>
-            <p>📍R. Mato Grosso, 612 -<br /> Vila Santana, Valinhos - SP, 13274-120</p>
-            <p>📞(19)98990-6423</p>
-            <p>📧tais.mobilizaamigos@gmail.com</p>
+        <div className="footer-column contacts-column">
+          <h4>Contatos</h4>
+          <p>
+            📍R. Mato Grosso, 612 -<br /> Vila Santana, Valinhos - SP, 13274-120
+          </p>
+          <p>📞(19) 98990-6423</p>
+          <p>📧tais.mobilizaamigos@gmail.com</p>
         </div>
-        <div class="footer-column social-column">
-            <h4>Siga nossas redes:</h4>
-            <a href="https://www.instagram.com/mobilizaamigos/" target="_blank" rel="noopener noreferrer">
-            <img src={logo-instagram.svg} alt="Mobiliza Amigos" />
+        <div className="footer-column social-column">
+          <h4>Siga nossas redes:</h4>
+          <a
+            href="https://www.instagram.com/mobilizaamigos/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={LogoInstagram} alt="Instagram Mobiliza Amigos" />
           </a>
-          <a href="https://www.tiktok.com/@mobiliza.amigos" target="_blank" rel="noopener noreferrer">
-            <img src={logo-tiktok.svg} alt="Mobiliza Amigos" />
+          <a
+            href="https://www.tiktok.com/@mobiliza.amigos"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={LogoTiktok} alt="TikTok Mobiliza Amigos" />
           </a>
         </div>
-    </div>
-    <div class="footer-bottom">
+      </div>
+      <div className="footer-bottom">
         © 2024 por BN Development | Política de Proteção de Dados
-    </div>
-  </footer>
-
-    )
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
