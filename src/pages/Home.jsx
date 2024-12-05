29c00d6b4dfa74e37828cbe37b9f273b115d5e8a
Bianca Nogueira <nogbianca@outlook.com>
2024-12-05 09:43:00 -0300

    Carrossel finalizado e separado do componente AboutUs para que eles sejam independentes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,17 @@ import Footer from "../components/Footer/Footer";
 import Button from "../components/Button/Button";
 import Banner from "../components/Banner/Banner";
 import Voluntariado from "../components/Voluntariado/Voluntariado";
+import AboutUs from "../components/AboutUs/AboutUs";
 
 function Home() {
     return (
         <>
             <Header />
-            <Button buttonStyle="outline">Olá</Button>
             <div className="home"> 
                 <Banner title="Doacoes" image="volunteer-background.svg"/>
             </div>
             <Voluntariado />
+            <AboutUs />
             <Footer />
         </>
     );
