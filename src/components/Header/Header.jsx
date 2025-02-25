98285d790c607facbf44eb54eddd8fa2a9a33817
Bianca Nogueira <nogbianca@outlook.com>
2025-02-25 19:03:22 -0300

    Adicionando responsividade na pagina de sobre nos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,49 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
-// ASSETS
 import './Header.css';
 import Logo from '../../assets/logo.svg';
+import { HiMenu, HiX } from 'react-icons/hi';
 
 function Header() {
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <header>
       <div className="container">
-        <div className="logo">
-          <Link to="/">
-            <img src={Logo} alt="Mobiliza Amigos" />
-          </Link>
-        </div>
+        {/* Condição para ocultar o logo quando o menu estiver ativo */}
+        {!menuActive && (
+          <div className="logo">
+            <Link to="/">
+              <img src={Logo} alt="Mobiliza Amigos" />
+            </Link>
+          </div>
+        )}
+    
         <nav>
-          <ul className="nav-links">
-            <li id='Home'><Link to="/">Início</Link></li>
+          <ul className={menuActive ? 'active' : ''}>
+            <li id="Home"><Link to="/">Início</Link></li>
             <li><Link to="/about">Quem Somos</Link></li>
             <li><Link to="/donations">Doações</Link></li>
             <li><Link to="/register">Inscrições</Link></li>
             <li><Link to="/contact">Contatos</Link></li>
           </ul>
         </nav>
-        <div className="cta-buttons">
-          <Link to="/donations" className="btn doar">Quero Doar</Link>
-          <Link to="/patrocinador" className="btn patrocinar">Quero Patrocinar</Link>
+
+                {/* Condição para ocultar os botões quando o menu estiver ativo */}
+                {!menuActive && (
+          <div className="cta-buttons">
+            <Link to="/donations" className="btn doar">Quero Doar</Link>
+            <Link to="/patrocinador" className="btn patrocinar">Quero Patrocinar</Link>
+          </div>
+        )}
+
+
+        <div className="menu-toggle" onClick={toggleMenu}>
+          {menuActive ? <HiX size={30} /> : <HiMenu size={30} />}
         </div>
       </div>
     </header>
