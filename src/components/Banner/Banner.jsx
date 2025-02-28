a489a14b62d029b0ad88b7b026105817b355742b
Bianca Nogueira <nogbianca@outlook.com>
2025-02-28 11:40:15 -0300

    Otimizando style do banner para melhor desempenho do site

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import '../Banner/Banner.css';
+import React from "react";
+import backgroundImg from "../../assets/background.webp";
+import "../Banner/Banner.css";
 
-function Banner(props) {
+function Banner() {
   return (
     <>
-      {/* Hero Section com fundo dinâmico usando a imagem passada como prop */}
-      <div className="hero displayFlex" style={{ backgroundImage: `url(${props.image})` }}>
+      {/* Hero Section com fundo otimizado */}
+      <div
+        className="hero displayFlex"
+        style={{
+          backgroundImage: `url(${backgroundImg})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
         <div className="container">
           <div className="hero-text">
-            <p>Conectamos sonhos e ações para transformar vidas e inspirar comunidades através do voluntariado.</p>
+            <p>
+              Conectamos sonhos e ações para transformar vidas e inspirar
+              comunidades através do voluntariado.
+            </p>
           </div>
         </div>
       </div>
